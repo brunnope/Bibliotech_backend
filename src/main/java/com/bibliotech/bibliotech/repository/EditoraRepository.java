7ade7c707b5ed0543e0cacf1a8e528f373f55248
Brunno Pereira <cicerobrnn111@gmail.com>
2025-06-12 21:07:00 -0300

    feat: criar repositories

---
+++ src/main/java/com/bibliotech/bibliotech/repository/EditoraRepository.java
@@ -0,0 +1,7 @@
+package com.bibliotech.bibliotech.repository;
+
+import com.bibliotech.bibliotech.entity.Editora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EditoraRepository extends JpaRepository<Editora, Long> {
+}
---
+++ src/main/java/com/bibliotech/bibliotech/repository/ExemplarRepository.java
@@ -0,0 +1,7 @@
+package com.bibliotech.bibliotech.repository;
+
+import com.bibliotech.bibliotech.entity.Exemplar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExemplarRepository extends JpaRepository<Exemplar, Long> {
+}
